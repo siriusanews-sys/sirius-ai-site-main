751c1361a8b7c3d6c9122c473ce8d918fe90e26f
SiriusAnews <siriusanews@gmail.com>
2026-05-16 01:06:42 +0300

    Sanitize liveFeed <img> src to string concatenation and remove placeholders

--- src/App.js
+++ src/App.js
@@ -1292,7 +1292,7 @@ function App() {
                 >
                   <div className="video-thumbnail-wrapper h-28 overflow-hidden bg-slate-900">
                     <img
-                      src={`https://youtube.com/${video.videoId}/mqdefault.jpg`}
+                      src={"https://youtube.com" + video.videoId + "/mqdefault.jpg"}
                       alt={video.title}
                       className="h-full w-full object-cover"
                       crossOrigin="anonymous"
